d6e2d15c9a87905511225770ff77ac98fcffffb7
JarCart <135964073+JarCart@users.noreply.github.com>
2024-04-29 10:24:30 -0400

    Add files via upload

--- Boothroyd_Vowels.m
+++ Boothroyd_Vowels.m
@@ -1,8 +1,9 @@
 close all; clear all; clc;
 addpath('C:\Users\Public\Documents\MATLAB codes');
-p_ref = 20/1000000;
-dB = 70;
-p = p_ref*10.^(dB/20);
+
+p_ref = 20/1000000; %sets reference for dB
+dB = 70; %Chooses initial intensity in dB
+p = p_ref*10.^(dB/20); %Sets intial intensity in dB
 
 %                      Some Common Vowels
 %               Vowel       f1      f2      f3
@@ -10,49 +11,52 @@ p = p_ref*10.^(dB/20);
 %                /i/        270    2290    3010
 %                /u/        300     870    2240
 
-dur= 0.800;
-Fs = 48828;
-F0 = 165;
-t = linspace(0,dur,Fs*dur);
-ramp=10; 
-order=60;
+dur= 0.800; %sets duration for vowl
+Fs = 48828; %sampling rate
+F0 = 150; %sets pitch at 150 Hz
+t = linspace(0,dur,Fs*dur); %Time-based values
+ramp=10; %ramps the stimuli for 10 ms
+order=60; % sets the filter order
 
+%The following sets the first vowel
     F1=270;
     F2=2290;
     F3=3010;
 
-ii=rampstim(MakeVowel(fix(dur*Fs),F0,Fs,F1,F2,F3),ramp,Fs);
+ii=rampstim(MakeVowel(fix(dur*Fs),F0,Fs,F1,F2,F3),ramp,Fs); %Makes the stimulus
 ii=0.7*ii/max(ii); %normalize    
-rmsii=sqrt(mean(ii.^2));
+rmsii=sqrt(mean(ii.^2)); %Gets stimulus RMS
 
     F1=300;
     F2=870;
     F3=2240;
 
-uu = rampstim(MakeVowel(fix(dur*Fs),F0,Fs,F1,F2,F3),ramp,Fs);
+uu = rampstim(MakeVowel(fix(dur*Fs),F0,Fs,F1,F2,F3),ramp,Fs); %Makes the stimulus
 uu = 0.7*uu/max(uu); %normalize
-rmsuu = sqrt(mean(uu.^2));
+rmsuu = sqrt(mean(uu.^2)); %Gets stimulus RMS
 
-setdBuu = (p/rmsuu)*uu;
-differences = [-5,-4,-3,-2,-1,0,1,2,3,4,5];
+setdBuu = (p/rmsuu)*uu; %Scales the /u/ stimulus to 70 dB
+differences = [-5,-4,-3,-2,-1,0,1,2,3,4,5]; %Intensity differences between the first and second vowels
 
-for r = 1:length(differences)
-    dB_ii = dB + differences(r);
-    p = p_ref*10.^(dB_ii/20);
-    setdBii = (p/rmsii)*ii;
+for r = 1:length(differences) %For all the differences we set
+    dB_ii = dB + differences(r); %Changes the intensity by the specific amount
+    p = p_ref*10.^(dB_ii/20); %references the new intensity
+    setdBii = (p/rmsii)*ii; %sets the intensity
 
-    stimuli(r,:) = [setdBuu,setdBii]; 
+    stimuli(r,:) = [setdBuu,setdBii]; %concatenates the /u/ with the /i/
 end
 
+%This next block saves the concatenated stimuli
 for i = 1:length(differences)
     if contains(string(differences(i)),'-')
-        stimName = strcat('D:\MartinBoothroydStim\ui_',string(differences(i)),'.wav');
+        stimName = strcat('D:\MartinBoothroydStim\ui_',string(differences(i)),'.wav'); %Change this to the file location
     else
-        stimName = strcat('D:\MartinBoothroydStim\ui_+',string(differences(i)),'.wav');
+        stimName = strcat('D:\MartinBoothroydStim\ui_+',string(differences(i)),'.wav');%Change this to the file location
     end
 audiowrite(stimName,stimuli(i,:),Fs)
 end
 
+%Does the same for the /u/ stimuli
 for r = 1:length(differences)
     dB_uu = dB + differences(r);
     p = p_ref*10.^(dB_uu/20);
